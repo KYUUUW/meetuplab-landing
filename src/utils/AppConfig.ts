5d99fb7ad87bed6b6dbc9806b8ea8f798c99424a
jangjaeko <jangjaeko45@gmail.com>
2023-09-08 18:05:41 +0900

    feat : change favicon to meetuplab temp logo

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,6 @@
+export const AppConfig = {
+  site_name: 'Meet Up Lab',
+  title: 'Meet Up Lab for Designer and Developer Blender',
+  description: '밋업랩에서 인사이트와 비즈니스 동료를 찾아보세요',
+  locale: 'ko-kr',
+};
